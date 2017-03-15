54246b266eab211cb5600ccfebe70a6b074b3a38
haozhang <hzhang428@gatech.edu>
2017-03-15 00:13:58 -0400

    Add Buttons and Change Styles

--- app/src/main/java/com/example/haozhang/yoo/MainActivity.java
+++ app/src/main/java/com/example/haozhang/yoo/MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.haozhang.yoo;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+@SuppressWarnings("ConstantConditions")
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,13 +17,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText editText = (EditText) findViewById(R.id.edit_text);
-        Button button = (Button) findViewById(R.id.button);
-        final TextView textView = (TextView) findViewById(R.id.text_view);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                textView.setText(editText.getText().toString());
-            }
-        });
+        final EditText editText = (EditText)findViewById(R.id.name);
+        Button[] buttons = new Button[4];
+        for (int i = 0; i < 4; i++) {
+            int id = getResources().getIdentifier("btn_" + (i+1), "id", getPackageName());
+            buttons[i] = (Button) findViewById(id);
+        }
+
+        for (int i = 0; i < 4; i++) {
+            buttons[i].setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    int duration = Toast.LENGTH_SHORT;
+                    CharSequence text = editText.getText().toString();
+                    Context context = getApplicationContext();
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+            });
+        }
+
     }
 }
