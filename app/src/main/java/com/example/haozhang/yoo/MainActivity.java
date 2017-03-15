a783a0dd91abc3895b5a454017ae30d373a0f261
haozhang <hzhang428@gatech.edu>
2017-03-14 23:44:59 -0400

    view input text via button

--- app/src/main/java/com/example/haozhang/yoo/MainActivity.java
+++ app/src/main/java/com/example/haozhang/yoo/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.haozhang.yoo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    //testing commit
+        final EditText editText = (EditText) findViewById(R.id.edit_text);
+        Button button = (Button) findViewById(R.id.button);
+        final TextView textView = (TextView) findViewById(R.id.text_view);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                textView.setText(editText.getText().toString());
+            }
+        });
+    }
 }
